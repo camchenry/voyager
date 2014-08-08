f1f6f94af90fb8188678644008d6963dc9060c43
camchenry <camchenry@users.noreply.github.com>
2014-08-07 23:10:42 -0400

    radar polish

--- entities/hud/radar.lua
+++ entities/hud/radar.lua
@@ -13,6 +13,11 @@ function Radar:update(dt)
 end
 
 function Radar:draw()
+    -- Radar circle
+    love.graphics.setColor(44, 44, 44, 200)
+    love.graphics.circle("fill", self.x, self.y, self.radius)
+
+    -- Radar objects and entities
     love.graphics.setColor(255, 255, 255)
     if the.system.objects ~= nil then
         for k, planet in pairs(the.system.objects) do
@@ -27,18 +32,49 @@ function Radar:draw()
 			
 			 -- Checks if the point is off the radar, and puts it on the edge if it is
 			local dist = math.dist(0, 0, radarX, radarY)
-			if dist > self.radius then
+            -- radius is off by a few pixels to compensate for the size of the radar dots
+			if dist > (self.radius - 3) then
 				local angle = math.angle(0, 0, radarX, radarY)
-				radarX = math.cos(angle) * self.radius
-				radarY = math.sin(angle) * self.radius
+
+                -- radius is off by a few pixels to compensate for the size of the radar dots
+				radarX = math.cos(angle) * (self.radius - 3)
+				radarY = math.sin(angle) * (self.radius - 3)
 			end
 			
+            love.graphics.setColor(99, 99, 99)
             love.graphics.circle("fill", self.x - radarX, self.y - radarY, 3)
-
-        -- needs entities too
 		end
     end
 
-    love.graphics.setColor(44, 44, 44, 200)
-    love.graphics.circle("fill", self.x, self.y, self.radius)
+    if the.system.entities ~= nil then
+        for k, entity in pairs(the.system.entities) do
+            local x, y = the.player.ship.body:getPosition()
+            local entX, entY = entity.x or entity.body:getX(), entity.y or entity.body:getY()
+
+            local dx = x - entX
+            local dy = y - entY
+            
+            -- Finds percentage of distance to an entity over range of the radar, then converts to radar location
+            local radarX = (dx/self.range)*self.radius
+            local radarY = (dy/self.range)*self.radius
+            
+             -- Checks if the point is off the radar, and puts it on the edge if it is
+            local dist = math.dist(0, 0, radarX, radarY)
+            -- radius is off by a few pixels to compensate for the size of the radar dots
+            if dist > (self.radius - 2) then
+                local angle = math.angle(0, 0, radarX, radarY)
+
+                -- radius is off by a few pixels to compensate for the size of the radar dots
+                radarX = math.cos(angle) * (self.radius - 2)
+                radarY = math.sin(angle) * (self.radius - 2)
+            end
+            
+            love.graphics.setColor(0, 0, 255)
+            love.graphics.circle("fill", self.x - radarX, self.y - radarY, 2)
+        end
+    end
+
+    -- center point
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.circle("fill", self.x, self.y, 1)
 end
