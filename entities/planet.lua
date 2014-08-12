b4cfb3a4acccdbe9e52546c6fd20d6421b689dbc
Nuthen <nuthen@comcast.net>
2014-08-12 13:17:35 -0400

    Added in online generated planets
    Doesn't work like intended

--- entities/planet.lua
+++ entities/planet.lua
@@ -13,11 +13,16 @@ function Planet:initialize()
 	
 	self.shader = love.graphics.newShader('shaders/planet.glsl')
 	--self.shader:send('rand', math.random())
-	self.time = 0
+	self.xtime = 0
+	self.ytime = 0
 	
 	self.seed = math.random(1000000)
 	
-	self.img = nil
+	self.img = love.graphics.newImage('img/planet/surface.png')
+	
+	self.shader:send('resolution', 1)
+	self.shader:send('width', 4)
+	self.shader:send('height', 1)
 	
 	self.color = {49, 193, 222}
 end
@@ -37,24 +42,27 @@ function Planet:isHabitable()
 end
 
 
-
+--[[
 function Planet:mapPixel(x, y, r, g, b, a)
     if r == 0 and g == 0 and b == 0 and a == 0 then
 		r, g, b, a = self.imgData:getPixel(x+1, y+1)
 	end
 	
     return r, g, b, a
-end
+end]]
 
 function Planet:update(dt)
-	self.time = self.time+dt/4
-	self.shader:send('time', self.time)
-
+	self.xtime = self.xtime+dt/4
+	self.ytime = self.ytime+dt/8
+	self.shader:send('xtime', self.xtime)
+	self.shader:send('ytime', self.ytime)
+ --[[
 	if not self.img then
 		self.imgData = love.image.newImageData(self.width, self.height)
 		
-		for x1 = 0, self.width-1 do
-			for y1 = 0, self.height-1 do
+		for x = 0, self.width-1 do
+			for y = 0, self.height-1 do
+				--------------
 				if x1 == 0 or x1 == self.width-1 or y1 == 0 or y1 == self.height-1 then
 					local angle = math.angle(x1, y1, self.radius+.5, self.radius+.5)
 				
@@ -86,6 +94,7 @@ function Planet:update(dt)
 						--	self.imgData:setPixel(x, y, 1, 0, 0, 0)
 						--	stopped = true
 						else
+						-----------
 							local noise1 = love.math.noise((self.x+x)/80, (self.y+y)/80, self.seed)
 							local noise2 = love.math.noise((self.x+x)/60, (self.y+y)/60, self.seed)
 							local noise3 = love.math.noise((self.x+x)/40, (self.y+y)/40, self.seed)
@@ -93,16 +102,19 @@ function Planet:update(dt)
 							
 							local noise = (noise1+noise2+noise3+noise4)/4
 							--value = value/2+noise/2
-							value = noise
+							local value = noise
 							
 							local value = x > self.radius and value or noise
-							--[[
-							r, g, b = 48, 227, 84
-							if value >= .75 then r, g, b = 158, 90, 22 
-							elseif value >= .5 then r, g, b = 222, 222, 67
-							elseif value >= .25 then r, g, b = 176, 227, 48
-							end
-							]]
+							
+							----
+							--r, g, b = 48, 227, 84
+							--if value >= .75 then r, g, b = 158, 90, 22 
+							--elseif value >= .5 then r, g, b = 222, 222, 67
+							--elseif value >= .25 then r, g, b = 176, 227, 48
+							--end
+							------
+							
+							
 							--local r, g, b = self.color[1], self.color[2], self.color[3]
 							--r, g, b = r/2+(value*255/2), g/2+(value*255/2), b/2+(value*255/2)
 							local r, g, b = 27, 79, 191
@@ -114,9 +126,9 @@ function Planet:update(dt)
 							end
 							
 							self.imgData:setPixel(x, y, r, g, b, 255)
-						end
-					end
-				end
+						--end
+					--end
+				--end
 			end
         end
 		
@@ -144,15 +156,14 @@ function Planet:update(dt)
 		end)
 		
 		self.img = love.graphics.newImage(self.imgData)
-	end
+	end]]
 end
-
 
 
 function Planet:draw()
     love.graphics.setColor(255, 255, 255)
 	love.graphics.setShader(self.shader)
     --love.graphics.circle("fill", self.x, self.y, self.radius)
-	love.graphics.draw(self.img, self.x-self.radius, self.y-self.radius)
+	love.graphics.draw(self.img, self.x-self.radius, self.y-self.radius, self.width/self.img:getWidth(), self.height/self.img:getHeight())
 	love.graphics.setShader()
 end
