6e9d1733e95a55e672d600cda9805af08f606cc3
Nuthen <nuthen@comcast.net>
2014-08-12 18:28:43 -0400

    img

--- entities/planet.lua
+++ entities/planet.lua
@@ -18,11 +18,19 @@ function Planet:initialize()
 	
 	self.seed = math.random(1000000)
 	
+<<<<<<< HEAD
 	--self.img = love.graphics.newImage('img/planet/surface.png')
 	
 	--self.shader:send('resolution', 1)
 	--self.shader:send('width', 4)
 	--self.shader:send('height', 1)
+=======
+	self.img = love.graphics.newImage('img/planet/surface.png')
+	
+	self.shader:send('resolution', 1)
+	self.shader:send('width', 4)
+	self.shader:send('height', 1)
+>>>>>>> parent of 26fb811... Revert "Added in online generated planets"
 	
 	self.color = {49, 193, 222}
 end
@@ -53,10 +61,16 @@ end]]
 
 function Planet:update(dt)
 	self.xtime = self.xtime+dt/4
+<<<<<<< HEAD
 	--self.ytime = self.ytime+dt/8
 	self.shader:send('time', self.xtime)
 	--self.shader:send('ytime', self.ytime)
 	
+=======
+	self.ytime = self.ytime+dt/8
+	self.shader:send('xtime', self.xtime)
+	self.shader:send('ytime', self.ytime)
+ --[[
 	if not self.img then
 		self.imgData = love.image.newImageData(self.width, self.height)
 		
@@ -79,6 +93,75 @@ function Planet:update(dt)
 				end
 				
 				self.imgData:setPixel(x, y, r, g, b, 255)
+				
+				--------------
+				if x1 == 0 or x1 == self.width-1 or y1 == 0 or y1 == self.height-1 then
+					local angle = math.angle(x1, y1, self.radius+.5, self.radius+.5)
+				
+					local value = 0
+					local stopped = false
+					local k = 1
+					while stopped == false do
+						k = k + 1
+						
+						local x = self.radius+.5 + math.cos(angle) * k
+						local y = self.radius+.5 + math.sin(angle) * k
+						
+						local remainderX = x%1
+						local remainderY = y%1
+						if remainderX >= .5 then
+							x = math.ceil(x)
+						else
+							x = math.floor(x)
+						end
+						if remainderY >= .5 then
+							y = math.ceil(y)
+						else
+							y = math.floor(y)
+						end
+						
+						if x < 0 or x > self.width-1 or y < 0 or y > self.height-1 then
+							stopped = true
+						--elseif math.dist(x, y, self.radius+.5, self.radius+.5) > self.radius then
+						--	self.imgData:setPixel(x, y, 1, 0, 0, 0)
+						--	stopped = true
+						else
+						-----------
+							local noise1 = love.math.noise((self.x+x)/80, (self.y+y)/80, self.seed)
+							local noise2 = love.math.noise((self.x+x)/60, (self.y+y)/60, self.seed)
+							local noise3 = love.math.noise((self.x+x)/40, (self.y+y)/40, self.seed)
+							local noise4 = love.math.noise((self.x+x)/20, (self.y+y)/20, self.seed)
+							
+							local noise = (noise1+noise2+noise3+noise4)/4
+							--value = value/2+noise/2
+							local value = noise
+							
+							local value = x > self.radius and value or noise
+							
+							----
+							--r, g, b = 48, 227, 84
+							--if value >= .75 then r, g, b = 158, 90, 22 
+							--elseif value >= .5 then r, g, b = 222, 222, 67
+							--elseif value >= .25 then r, g, b = 176, 227, 48
+							--end
+							------
+							
+							
+							--local r, g, b = self.color[1], self.color[2], self.color[3]
+							--r, g, b = r/2+(value*255/2), g/2+(value*255/2), b/2+(value*255/2)
+							local r, g, b = 27, 79, 191
+							if value > .9 then r, g, b = 39, 115, 24
+							elseif value > .7 then r, g, b = 55, 184, 64
+							elseif value > .54 then r, g, b = 235, 235, 171
+							elseif value > .5 then r, g, b = 168, 201, 240
+							elseif value > .4 then r, g, b = 106, 130, 222
+							end
+							
+							self.imgData:setPixel(x, y, r, g, b, 255)
+						--end
+					--end
+				--end
+>>>>>>> parent of 26fb811... Revert "Added in online generated planets"
 			end
         end
 		
@@ -112,7 +195,7 @@ function Planet:update(dt)
 		end)
 		
 		self.img = love.graphics.newImage(self.imgData)
-	end
+	end]]
 end
 
 
@@ -120,6 +203,6 @@ function Planet:draw()
     love.graphics.setColor(255, 255, 255)
 	love.graphics.setShader(self.shader)
     --love.graphics.circle("fill", self.x, self.y, self.radius)
-	love.graphics.draw(self.img, self.x-self.radius, self.y-self.radius)
+	love.graphics.draw(self.img, self.x-self.radius, self.y-self.radius, self.width/self.img:getWidth(), self.height/self.img:getHeight())
 	love.graphics.setShader()
 end
