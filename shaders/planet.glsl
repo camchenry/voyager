26fb8113234c4828312dc238ab519225da844b13
Nuthen <nuthen@comcast.net>
2014-08-12 14:05:31 -0400

    Revert "Added in online generated planets"
    This reverts commit b4cfb3a4acccdbe9e52546c6fd20d6421b689dbc.

--- shaders/planet.glsl
+++ shaders/planet.glsl
@@ -1,8 +1,7 @@
 const number pi = 3.14159265;
 const number pi2 = 2.0 * pi;
 
-extern number xtime;
-extern number ytime;
+extern number time;
 extern number resolution = 1;
 
 extern number width = 1;
@@ -10,14 +9,22 @@ extern number height = 1;
 			  
 vec4 effect(vec4 color, Image texture, vec2 tc, vec2 pixel_coords)
 {
-   vec2 p = 2.0 * (tc - 0.5);							// center on canvas
-   number r = sqrt(p.x*width*p.x + p.y*height*p.y);		// sphere size
+   vec2 p = 2.0 * (tc - 0.5);
+   
+   number r = sqrt(p.x*width*p.x + p.y*height*p.y);
+
    if (r > 1.0) discard;
+   
    number d = r != 0.0 ? asin(r) / r : 0.0;
+			
    vec2 p2 = d * p * resolution;
-   number x3 = mod(p2.x / pi2 + 0.5 + xtime, 1.0);
-   number y3 = mod(p2.y / pi2 + 0.5 + ytime, 1.0);
-   vec2 newCoord = vec2(x3, y3);						// location of texture on sphere
+   
+   number x3 = mod(p2.x / (pi2) + 0.5 + time/8, 1.0);
+   number y3 = p2.y / (pi2) + 0.5;
+   
+   vec2 newCoord = vec2(x3, y3);
+   
    vec4 sphereColor = color * Texel(texture, newCoord);
+			
    return sphereColor;
 }
